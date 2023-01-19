7578abbd7056805c91870f1fd4bce7922a342fa5
Anton Filatov <antartbox@gmail.com>
2023-01-19 12:47:30 +0400

    Initial commit

---
+++ src/main/java/itmo/java/advanced_124_31/AdvancedApplication.java
@@ -0,0 +1,13 @@
+package itmo.java.advanced_124_31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedApplication.class, args);
+	}
+
+}
