478944299f8c4eaa1b071854ad106f6540ac05b8
Anton Filatov <antartbox@gmail.com>
2023-02-07 20:30:57 +0400

    minor fix

--- src/test/java/itmo/java/advanced_124_31/controllers/CarControllerTest.java
+++ src/test/java/itmo/java/advanced_124_31/controllers/CarControllerTest.java
@@ -2,16 +2,19 @@ package itmo.java.advanced_124_31.controllers;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import itmo.java.advanced_124_31.model.entity.Car;
-import lombok.SneakyThrows;
+import itmo.java.advanced_124_31.model.enums.CarClass;
+import itmo.java.advanced_124_31.model.enums.CarStatus;
+import itmo.java.advanced_124_31.model.enums.Color;
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,54 +22,63 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+//@AutoConfigureMockMvc
 public class CarControllerTest {
 	@Rule
 	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation(
 			"target/generated-snippets");
+	Car car = new Car();
+	//@Autowired
 	MockMvc mockMvc;
 	@Autowired
 	WebApplicationContext webApplicationContext;
 	@Autowired
 	ObjectMapper objectMapper;
-	Car car = new Car();
 
-	//{
-	//	car.setId(2L);
-	//	car.setVehicleYear(2010);
-	//	car.setName("LADA");
-	//	car.setColor(Color.BLACK);
-	//	car.setStateNumber("A111AА111");
-	//	car.setSeats(4);
-	//}
+	{
+		car.setId(1L);
+		car.setVehicleYear(2010);
+		car.setName("LADA");
+		car.setColor(Color.BLACK);
+		car.setStateNumber("A111AА111");
+		car.setSeats(4);
+		car.setBabyChair(false);
+		car.setCarClass(CarClass.COMFORT);
+		car.setStatus(CarStatus.CREATED);
+		//car.setCreatedAt(LocalDateTime.now());
+		//car.setUpdatedAt(LocalDateTime.now());
+	}
 
 	@Before
 	public void setUp() {
 
-		ConfigurableMockMvcBuilder builder =
-				MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
-						.alwaysDo(document(" {method-name}/{step}/"))
-						.apply(documentationConfiguration(this.restDocumentation));
+		ConfigurableMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(
+						this.webApplicationContext).alwaysDo(document(" {method-name}/{step}/"))
+				.apply(documentationConfiguration(this.restDocumentation));
 		this.mockMvc = builder.build();
 	}
 
 	@Test
-	@SneakyThrows
-	public void create() {
+	@Transactional
+	//@SneakyThrows
+	public void create() throws Exception {
 
 		String content = objectMapper.writeValueAsString(car);
 		System.out.println(content);
 		String uri = "/cars";
+
 		mockMvc.perform(
-						MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON)
-								.content(content)).andDo(print()).andExpect(status().isCreated())
-				.andDo(print()).andExpect(jsonPath("$.vehicleYear").value(2010))
+						post(uri).contentType(MediaType.APPLICATION_JSON).content(content))
+				.andDo(print()).andExpect(status().isCreated()).andDo(print())
+				//.andExpect(status().isCreated()).andDo(print())
+				.andExpect(jsonPath("$.vehicleYear").value(2010))
 				.andExpect(jsonPath("$.name").value("LADA"))
 				.andExpect(jsonPath("$.stateNumber").value("A111AА111"))
 				.andExpect(jsonPath("$.seats").value("4"))
